8eeed07597b5298037788d71d4049eebf1931b2d
Julio Cesar Momente <juliocesar.momente@gmail.com>
2023-06-01 21:46:43 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string name = "  Julio Cesar Momente   ";
+Console.WriteLine($"Olá, [{name}]");
+string fName = name.Trim();
+System.Console.WriteLine($"Este é seu nome sem espaços: [{fName}]");
+System.Console.WriteLine($"Seu nome tem {fName.Length} letras.");
+System.Console.WriteLine($"Tudo maiúsculo: {fName.ToUpper()} | tudo minúsculo: {fName.ToLower()}");
+System.Console.WriteLine($"Trocando os espaços por traços: {fName.Replace(' ','-')}");
